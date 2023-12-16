2e5cc757f8ed066f1e4b3686d025917a274d8652
Jarod <schuppjarod@gmail.com>
2023-12-15 19:49:39 -0500

    filter search

--- front-end/src/components/Filter.jsx
+++ front-end/src/components/Filter.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import FilterSVG from '@material-symbols/svg-500/outlined/filter_alt.svg?react';
 
+
 function Filter({ onDegreeSelect }) {
+    const [originalDegrees, setOriginalDegrees] = useState([]);
     const [degrees, setDegrees] = useState([]);
 
     useEffect(() => {
         const fetchDegrees = async () => {
             try {
                 const response = await axios.get('http://localhost:8080/api/degrees/names');
-                setDegrees(response.data);
+                setOriginalDegrees(response.data); // Set original degrees
+                setDegrees(response.data); // Also set degrees for initial render
             } catch (error) {
                 console.error('Error fetching degrees:', error);
             }
@@ -18,11 +21,31 @@ function Filter({ onDegreeSelect }) {
         fetchDegrees();
     }, []);
 
+    const handleSearchChange = (event) => {
+        const term = event.target.value.toLowerCase();
+        const filteredDegrees = originalDegrees.filter(degree =>
+            degree.degreeName.toLowerCase().includes(term)
+        );
+        setDegrees(filteredDegrees);
+    };
+
     return (
         <div className="dropdown dropdown-bottom dropdown-end">
             <FilterSVG tabIndex={0} role="button" className="fill-secondary h-10 w-10"></FilterSVG>
             <ul tabIndex={0} className="dropdown-content menu flex-nowrap p-2 shadow bg-base-100 rounded-box my-2 w-80 h-56 overflow-y-auto">
                 <h1 key="title" className="flex items-center justify-center">Sort By Degree</h1>
+
+                <div className="justify-center flex">
+                    <div className="form-control">
+                        <input
+                            type="text"
+                            placeholder="Search By Code or Name"
+                            className="input input-bordered w-56 m-4 h-10 md:w-auto"
+                            onChange={handleSearchChange}
+                        />
+                    </div>
+                </div>
+
                 {degrees.map((degree, index) => (
                     <li key={index} onClick={() => onDegreeSelect(degree.degreeId)}>
                         <a className="text-white">{degree.degreeName}</a>
@@ -34,3 +57,4 @@ function Filter({ onDegreeSelect }) {
 }
 
 export default Filter;
+
